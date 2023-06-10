ad0786c07d711acafafbc19fcdb5eb2004ffe7c5
julianderson <jsoares.4720@gmail.com>
2023-06-10 13:43:53 -0300

    .

--- poo/Getters and Setters.php
+++ poo/Getters and Setters.php
@@ -1,8 +1,20 @@
 <?php
-class Login{
+    class Login{
     public $email;
     public $senha;
+    public function getEmail(){
+        return $this->email;
+    }
+    public function setEmail($e){
+        $this->email=$e;
+    }
 
+    public function getSenha(){
+        return $this->senha;
+    }
+    public function setSenha($s){
+        $this->senha=$s;
+    }
     public function Logar(){
         if ($this->email == "teste@teste.com" and $this->senha=="123456"):
             echo "Logado com sucesso!";
@@ -14,4 +26,10 @@ class Login{
     }
     
 }
+$logar = new Login();
+$logar->setEmail("teste@teste.com");
+$logar->setSenha("1234567");
+$logar->Logar();
+echo "<br>";
+echo $logar->getEmail();
 ?>
