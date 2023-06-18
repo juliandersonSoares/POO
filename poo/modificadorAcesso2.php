f311f1fb98d52603ff6005767a694743ee7002af
julianderson <jsoares.4720@gmail.com>
2023-06-18 18:20:07 -0300

    introdução a modificadores de acesso pt2

---
+++ poo/modificadorAcesso2.php
@@ -0,0 +1,25 @@
+<?php
+class Veiculo{
+    public $modelo;
+    public $cor ;
+    public $ano;
+
+    private function Andar(){ echo "Andou";}
+    public function Parar(){ echo "Parou";}
+
+    public function mostrarAcao(){
+        $this->Andar();
+    }
+
+
+}
+
+class Carro extends Veiculo{
+    public function ligarLimpador(){
+        echo "Limpando em 321";
+    }
+}
+
+$carro= new Carro();
+$carro->mostrarAcao(); 
+?>
